9f3e2f94065fea58e270f3fa4c6bef11e1bcf1a2
Aryan <alikhaniaryan@live.com>
2018-04-23 04:38:40 +0430

    documenting

--- app/config.py
+++ app/config.py
@@ -1,17 +1,13 @@
 from flask import Flask, send_file
 import os
 
+# initializing flask app
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
+app.config['SECRET_KEY'] = 'some secret key'
 
 import routes
 
-
-@app.errorhandler(IOError)
-def handle_invalid_usage(error):
-    return 'error'
-
-
+# serve static file and folders inside public directory
 @app.route('/<path:path>')
 def static_file(path):
     temp = os.path.dirname(os.path.realpath(__file__)) + '/public/' + path
--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,5 @@
+# routes
+
 from config import app
 from flask import render_template, jsonify
 import threading
@@ -8,47 +10,69 @@ _lastTime = 0
 _alive = False
 _data = 0
 _semiLock = threading.Semaphore()
+_running = 0
+
 
 
 @app.before_request
 def before_request():
+    # close the existing thread on background everytime that route configuration changed
     global _alive, _lastTime, _data, _semiLock
     if _alive:
         with _semiLock:
             _alive = False
-        sleep(0.2)
 
 
 def handler():
+    # handle somthing synchrounus, this is an example non blocking operation in sync mode
     global _data, _alive, _lastTime, _semiLock
-
+    print '>> thread'
     while _alive:
         if time() - _lastTime > 10:
             with _semiLock:
                 _data = 19 + _data
                 _alive = False
 
+        sleep(0.3) # delay must be lower than the value from origin thread kill
+    print '<< thread'
+
+
 
 @app.route('/activate', methods=['POST'])
 def activate():
-    global _alive, _lastTime, _data, _semiLock
-    tmp = randint(1000, 9999)
-    print(str(tmp) + ' activated')
+    global _alive, _lastTime, _data, _semiLock, _running
 
+    _running = _running + 1
+
+
+    tmp = randint(10000, 99999)
+    print('/activate --> thread ' + str(tmp) + ' started --> ' + str(_running))
+
+    # origin thread kill
+    # check if thread is alive, kill it
     if _alive:
+        # do it with semaphore to keep objects aligned
         with _semiLock:
             _alive = False
+        # wait for thread to get killed.
         sleep(0.5)
 
+    # change state of thread to alive
     with _semiLock:
         _alive = True
 
+
+    # run a thread
     _lastTime = time()
     trd = threading.Thread(target=handler)
     trd.start()
+    # and join it current context to make interpreter stay on this current line
     trd.join()
 
-    print(str(tmp) + ' deativated')
+
+    _running = _running - 1
+
+    print('/activate <-- thread ' + str(tmp) + ' stopped <-- ' + str(_running))
     return jsonify({'name': 'hello', 'data': _data})
 
 
